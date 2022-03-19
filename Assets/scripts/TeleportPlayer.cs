58c63b05fa5f3bca7a72a690831ff381a2931e46
vlad980000 <vlad980000@mail.ru>
2022-03-19 23:53:21 +0300

    init

---
+++ Assets/scripts/TeleportPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    [SerializeField]private GameObject pointTeleport;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = pointTeleport.gameObject.transform.position;
+        }
+    }
+}
---
+++ Assets/scripts/Voise.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voise : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _duration;
+
+    private float _volumeScale;
+    private float _runningTime;
+    private float _target = 1f;
+    void Start()
+    {
+        _audioSource.volume = 1f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            _audioSource.Play();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        _runningTime += Time.deltaTime;
+        _volumeScale = _runningTime / _duration;
+
+        if(_audioSource.volume == 1f)
+        {
+            _target = 0f;
+            _runningTime = 0;
+        }
+        else if(_audioSource.volume == 0f)
+        {
+            _target = 1f;
+            _runningTime = 0;
+        }
+
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _target, _volumeScale);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _audioSource.Stop();
+    }
+}
